0064126472a253bb6ebc290a2d397172e52dceca
Flavio Rossi <flaviowenzel1996@gmail.com>
2022-11-07 13:02:44 -0300

    Tempo limite de expiração do token aumentado

--- src/FriGado.API/Controllers/LoginController.cs
+++ src/FriGado.API/Controllers/LoginController.cs
@@ -1,6 +1,5 @@
 ﻿using FriGado.API.Domain;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
